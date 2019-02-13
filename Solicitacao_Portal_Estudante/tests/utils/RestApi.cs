f1a8a4415636ef00ce7671a355df63383df6fea5
leonardoBarcellosM <leonardo.barcellos@viaflow.com.br>
2019-02-13 09:54:14 -0200

    Adicionados testes para verificar o funil do portal da IES 03

--- Solicitacao_Portal_Estudante/tests/utils/RestApi.cs
+++ Solicitacao_Portal_Estudante/tests/utils/RestApi.cs
@@ -10,20 +10,20 @@ namespace Solicitacao_Portal_Estudante.tests.steps
 {
     class RestApi
     {
-        static HttpClient Client = new HttpClient();
+        static HttpClientHandler Client = new HttpClientHandler();
 
-        public bool ConsultarToken()
-        {
-            bool _result = true;
-            string Path = "http://homologacao.fundacred.org.br:8080/portal-estudante-rest/resources/acao/buscar?sort=-dtAcao&filter=cpf=80220450048";
+        //public bool ConsultarToken()
+        //{
+        //    bool _result = true;
+        //    string Path = "http://homologacao.fundacred.org.br:8080/portal-estudante-rest/resources/acao/buscar?sort=-dtAcao&filter=cpf=80220450048";
 
-            var result = Client.GetStringAsync(new Uri(Path)).Result;
+        //    var result = Client.GetStringAsync(new Uri(Path)).Result;
 
-            string[] jSon = result.Split('\"');
-            result = jSon[5];
-            //return result;
-            return _result;
-        }
+        //    string[] jSon = result.Split('\"');
+        //    result = jSon[5];
+        //    //return result;
+        //    return _result;
+        //}
 
     }
 }
